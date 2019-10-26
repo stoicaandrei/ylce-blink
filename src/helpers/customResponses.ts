ffd96849feae0167b1763661e46dcc99bfcddad5
Andrei Stoica <andrei@eumeo.io>
2019-10-26 15:19:18 +0300

    changed default status code for error

--- src/helpers/customResponses.ts
+++ src/helpers/customResponses.ts
@@ -7,7 +7,7 @@ const success = function (this: Response, data: any = null) {
   });
 }
 
-const error = function (this: Response, err: any = 'Unexpected error occurred.', status = 500) {
+const error = function (this: Response, err: any = 'Unexpected error occurred.', status = 400) {
   this.status(status).send({
     success: false,
     message: (err instanceof Error ? err.message : err.toString()),
